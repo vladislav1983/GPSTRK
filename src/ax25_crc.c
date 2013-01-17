05a9cb33cf95b3eeb6a6b31dfe9ddd0ba22e6e5f
vladislav.gyurov <vladislav.gyurov@gmail.com>
2013-01-17 23:41:58 +0000

    AX25 CRC module cleanup.

--- src/ax25_crc.c
+++ src/ax25_crc.c
@@ -79,12 +79,6 @@ static const U16 u16Crc16Table[256] =
     0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330,
     0x7bc7, 0x6a4e, 0x58d5, 0x495c, 0x3de3, 0x2c6a, 0x1ef1, 0x0f78
 };
-#elif(AX25_CRC_TINY_TABLE_CALC == 1)
-static const U16 u16Crc16Table[16] = 
-{
-    0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
-    0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7
-};
 #endif
 /*=====================================================================================================================
  * Constant exported data                                                     
@@ -163,7 +157,7 @@ U16 Crc_Ax25Calc(U8* pu8Buff, U16 u16Size)
     return u16Crc;
 }
 #else
-    #error "Are you crazy?"
+    #error "AX25 CRC calculation algotithm must be defined!"
 #endif
 
 /*=====================================================================================================================
